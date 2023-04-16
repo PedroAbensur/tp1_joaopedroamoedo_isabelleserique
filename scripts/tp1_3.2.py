f1c9cdb6312754d626a1524fac8fcd67a0701307
PedroAbensur <jpaamoedo@gmail.com>
2023-04-16 18:43:42 -0400

    Reading products and similars

--- scripts/tp1_3.2.py
+++ scripts/tp1_3.2.py
@@ -84,3 +84,26 @@ class catprod:
         print("=====================================================")
         print(f"asin_product: {self.asin_product}")
         print(f"cat_id = {self.cat_id}")
+
+f = open("amazon-meta.txt", "+r")
+f.readline() #skip comment
+items = f.readline().split()[2]
+f.readline() #skip blank line
+for _ in range(int(items)):
+    id = f.readline().split()[1]
+    asin = f.readline().split()[1]
+    line = f.readline().split()
+    if(line[0] == 'discontinued'):
+        prod = product(id, asin, 'NULL', 'NULL', 'NULL')
+    else:
+        #
+        title = ' '.join(line[1:]).replace("'","''")
+        group = f.readline().split()[1]
+        salesrank = f.readline().split()[1]
+        prod = product(id, asin, title, group, salesrank)
+
+        #reading similar
+        line = f.readline().split()
+        similar_list = line[2:]
+        for i in range(int(line[1])):
+            sim = similar(prod.asin, similar_list[i])
